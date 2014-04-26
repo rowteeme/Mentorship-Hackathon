07eaf59c10d34e2d16fa77592789f642d68fb9fd
rowteeme <rotimi.oyewole@gmail.com>
2014-04-26 19:57:21 -0400

    Polling for merchant data on prototype

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,47 @@
 var hackApp = angular.module('hackApp', []);
 
-hackApp.controller('SearchCtrl', ['$scope', '$http', function ($scope) {
+hackApp.controller('SearchCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.prompt = 'What kind of cuisine would you like? (ex: American, Mexican, Chinese)';
     $scope.searchQuery = {};
 
 
+        $scope.searchQuery.results = {};
+        // $scope.searchQuery.merchants = {};
+        // $scope.searchQuery.overall_rating = {};
 
+
+
+    $http({
+        method: 'GET',
+        url: '//localhost:3000/search'
+    })
+
+    .success( function (data, status, headers, config) {
+
+        // JSON doesn't support .length, so setting a key for the object and assigning to count gives it a length.
+        var count = (Object.keys(data.merchants).length);
+
+        for(i=0; i <= count; i++) {
+           $scope.searchQuery.results = data.merchants[i].summary.cuisines;
+           console.log($scope.searchQuery.results);
+        }
+
+        // $scope.searchQuery.restaurants =
+        // $scope.searchQuery.score = data.merchants.summary.overall_rating;
+        console.log('it worked');
+
+        // console.log($scope.searchQuery.score);
+    })
+
+    .error( function (data, status, headers, config) {
+        console.log('It did not work');
+    });
+
+//     $scope. = function(merchant) {
+//     return function(user) {
+//         return restaurant.id;
+//     }
+// }
 
 }]);
 hackApp.controller('OrderCtrl', ['$scope', function ($scope) {}]);
