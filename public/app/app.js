75f2a62172ec716f20197848816d46e905cd1e6f
MaxOfferpop <mbaldwin@offerpop.com>
2014-04-30 22:35:59 -0400

    Moved final files into the repository
    Merged the files we presented with and this repository

--- public/app/app.js
+++ public/app/app.js
@@ -2,16 +2,10 @@ var hackApp = angular.module('hackApp', []);
 
 hackApp.controller('SearchCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.prompt = 'What kind of cuisine would you like? (ex: American, Mexican, Chinese)';
-    $scope.searchQuery = {};
-
-
-        $scope.searchQuery.results = {};
-        $scope.searchQuery.merchants = {};
-        $scope.searchQuery.results.id = '';
-        // $scope.searchQuery.overall_rating = {};
-        $scope.searchQuery.restaurantResults = {};
-        $scope.searchQuery.restuarantID = '';
-
+    $scope.query = {};
+    $scope.queryBy = '$';
+    $scope.restaurants = [];
+    $scope.ID = '';
 
 
     $http({
@@ -20,37 +14,24 @@ hackApp.controller('SearchCtrl', ['$scope', '$http', function ($scope, $http) {
     })
 
     .success( function (data, status, headers, config) {
-
-        // JSON doesn't support .length, so setting a key for the object and assigning to count gives it a length.
-        var count = (Object.keys(data.merchants).length);
-
-        for(i=0; i <= count; i++) {
-           $scope.searchQuery.results = data.merchants[i];
-           $scope.restaurantResults = [];
-            // angular.forEach($scope.restaurantResults, function( id, summary){
-            //     $scope.restaurantResults.push({id: id, summary: summary});
-            // });
-           console.log($scope.searchQuery.results.id);
-           console.log($scope.searchQuery.results.summary.cuisines);
-           // console.log($scope.restaurantResults.summary);
-        }
-
-        // $scope.searchQuery.restaurants =
-        // $scope.searchQuery.score = data.merchants.summary.overall_rating;
-        console.log('it worked');
-
-        // console.log($scope.searchQuery.score);
-    })
-
-    .error( function (data, status, headers, config) {
-        console.log('It did not work');
+        console.log(data);
+        $scope.restaurants = data.merchants;
+        $scope.ID = data.merchants.id;
+        })
+    .error( function(data, status, headers, config) {
+        console.log('Error');
     });
 
-//     $scope. = function(merchant) {
-//     return function(user) {
-//         return restaurant.id;
-//     }
-// }
+    // $http({
+    //     method: 'GET',
+    //     url: '//localhost:3000/menu/90'
+    // })
+
+    // .success ( function (data, status, headers, config) {
+    //     // $scope.restaurants.id = data.id;
+    //     console.log(data);
+
+    // });
 
 }]);
 hackApp.controller('OrderCtrl', ['$scope', function ($scope) {}]);
